5ab471e30e99eb4dbb2762c21e24a9a51eebbaa8
Jisun Park <fpdldk912@gmail.com>
2020-03-22 17:00:23 +0900

    Added views to create/edit blog post inside the site.

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 def post_list(request):
@@ -10,3 +11,31 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+def post_new(request):
+    if request.method == "POST": # 폼에 데이터를 입력한 경우
+        form = PostForm(request.POST) # 폼에서 받은 데이터 가져오기
+        if form.is_valid():
+            post = form.save(commit=False) # 받은 데이터를 바로 Post모델에 저장하지 말기
+            post.author = request.user # author 추가
+            post.published_date = timezone.now() # publish_date 현재시간으로 추가
+            post.save() # 변경사항 저장
+            return redirect('post_detail', pk=post.pk) # 저장 후 post_detail 뷰로 이동
+    else: # 처음 페이지 접속
+        form = PostForm() # 비어있는 폼 제공
+    return render(request, 'blog/post_edit.html', {'form':form})
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk) # Post 모델 가져오기
+    if request.method == "POST": # 수정된 글을 입력한 경우
+        form = PostForm(request.POST, instance=post) # 수정된 데이터를 가져오기
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else: # 글을 수정하려고 처음 버튼을 눌렀을 때
+        form = PostForm(instance=post) # 작성되었던 글을 불러오는 것
+    return render(request, 'blog/post_edit.html', {'form': form})
+            
