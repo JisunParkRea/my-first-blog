6d4ebf8eff405e5fcc53b71e854507b5295fbc00
Jisun Park <fpdldk912@gmail.com>
2020-03-31 20:48:33 +0900

    Add comment model, view, url to make comments in blog post

--- blog/models.py
+++ blog/models.py
@@ -15,6 +15,23 @@ class Post(models.Model):
         self.published_date = timezone.now()
         self.save()
 
+    def approved_comments(self):
+        return self.comments.filter(approved_comment=True)
+
     # Post 모델의 제목을 알려주는 메소드
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+    post = models.ForeignKey('blog.Post', on_delete=models.CASCADE, related_name='comments') #related_name 옵션을 통해 Post 모델에서 댓글에 access하게 함
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def __str__(self):
+        return self.text
